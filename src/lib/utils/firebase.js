3bd3881f30c4615f4b654d33040867a331a2824d
École du Web <ledesignerduweb@outlook.fr>
2024-12-27 18:05:09 +0100

    add firebase keys in .env.local

--- src/lib/utils/firebase.js
+++ src/lib/utils/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqeEoFbbrWgSKMQdmUaekj7AebwiMnvsw",
-  authDomain: "axoria-blog.firebaseapp.com",
-  projectId: "axoria-blog",
-  storageBucket: "axoria-blog.appspot.com",
-  messagingSenderId: "305972934115",
-  appId: "1:305972934115:web:cb86225f459524f322dcd6",
-  measurementId: "G-JZLG823WYZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
